485dc80fc59130773e61dee287ad8c14f5e9210a
anant-mittal123 <anantmittal410@gmail.com>
2026-06-01 23:12:36 +0530

    Add program to check for Strong Number

---
+++ Day_5/program2.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+
+int main() {
+    int n, temp, digit, sum = 0, i, fact;
+
+    scanf("%d", &n);
+
+    temp = n;
+
+    while (temp > 0) {
+        digit = temp % 10;
+        fact = 1;
+
+        for (i = 1; i <= digit; i++)
+            fact *= i;
+
+        sum += fact;
+        temp /= 10;
+    }
+
+    if (sum == n)
+        printf("Strong Number");
+    else
+        printf("Not Strong Number");
+
+    return 0;
+}
