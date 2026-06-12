8e18fc132d07627a6d9be16af0d1586cff1e8521
anant-mittal123 <anantmittal410@gmail.com>
2026-06-12 09:32:37 +0530

    Add program to find maximun frequency of element
    This program reads an array of integers and finds the most frequent element.

---
+++ day_16/program2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main()
+{
+    int a[100], n, i, j;
+    int maxCount = 0, element, count;
+
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    for(i = 0; i < n; i++)
+    {
+        count = 1;
+
+        for(j = i + 1; j < n; j++)
+        {
+            if(a[i] == a[j])
+                count++;
+        }
+
+        if(count > maxCount)
+        {
+            maxCount = count;
+            element = a[i];
+        }
+    }
+
+    printf("Element = %d", element);
+
+    return 0;
+}
