9b7fbd9ced5c5951453204de39447d6c52fee2d5
anant-mittal123 <anantmittal410@gmail.com>
2026-06-15 11:30:16 +0530

    Add program to calculate diagonal sum of matrix

---
+++ Day_19/program4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[10][10];
+    int i, j, n, sum = 0;
+
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++)
+    {
+        for(j = 0; j < n; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for(i = 0; i < n; i++)
+    {
+        sum = sum + a[i][i];
+    }
+
+    printf("Diagonal Sum = %d", sum);
+
+    return 0;
+}
