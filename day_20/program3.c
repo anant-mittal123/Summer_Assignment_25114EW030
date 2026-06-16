73602761da3a612d6c81164386d6f382ebe58377
anant-mittal123 <anantmittal410@gmail.com>
2026-06-16 09:16:02 +0530

    Add program to calculate row sums of a matrix

---
+++ day_20/program3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[10][10];
+    int i, j, r, c, sum;
+
+    scanf("%d %d", &r, &c);
+
+    for(i = 0; i < r; i++)
+    {
+        for(j = 0; j < c; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for(i = 0; i < r; i++)
+    {
+        sum = 0;
+
+        for(j = 0; j < c; j++)
+        {
+            sum = sum + a[i][j];
+        }
+
+        printf("Row %d Sum = %d\n", i + 1, sum);
+    }
+
+    return 0;
+}
