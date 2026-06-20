54f32f134faecf74729117c3700ca278d2dbcd60
anant-mittal123 <anantmittal410@gmail.com>
2026-06-20 13:15:33 +0530

    Implement string rotation check in program1.c

---
+++ day_24/program1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char s1[100], s2[100], temp[200];
+
+    scanf("%s", s1);
+    scanf("%s", s2);
+
+    strcpy(temp, s1);
+    strcat(temp, s1);
+
+    if(strstr(temp, s2))
+        printf("Rotation");
+    else
+        printf("Not Rotation");
+
+    return 0;
+}
